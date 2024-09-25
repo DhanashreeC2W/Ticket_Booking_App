3be308a6fb021cb5de1b71c05dd714ae246db115
DhanashreeC2W <dhanashreeflutter@gmail.com>
2024-09-25 22:16:23 +0530

    confirm pin screen done

---
+++ lib/view/confirm_pin_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_booking_app/view/widget/number_pin_widget.dart';
+
+class ConfirmPinScreen extends StatelessWidget {
+  const ConfirmPinScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromRGBO(255,255,255,1),
+        body: const NumberPinWidget(
+            pinType: "Confirm \nyour PIN",
+            subTitle:
+                "Enter 6 digits number for your PIN. Make sure the combination is not easy to guess."));
+  }
+}
